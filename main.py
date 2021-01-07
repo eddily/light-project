0c817a2a11aa7db78168bbf175599445062f4cf7
eddily <72766373+eddily@users.noreply.github.com>
2021-01-06 19:46:24 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -2,16 +2,78 @@ while True:
     print("temperature " + input.temperature(TemperatureUnit.FAHRENHEIT))
     print("light level " + input.light_level())
 
-    if input.temperature(TemperatureUnit.FAHRENHEIT) > 80 and input.light_level() > 50:
-        light.set_all(color.rgb(255, 0, 0))
-        print("hot and sunny")
+    if input.temperature(TemperatureUnit.FAHRENHEIT) > 70:
+        light.set_pixel_color(0, color.rgb(255, 0, 0))
+        light.set_pixel_color(1, color.rgb(255, 0, 0))
+        light.set_pixel_color(2, color.rgb(255, 0, 0))
+        light.set_pixel_color(3, color.rgb(255, 0, 0))
+        light.set_pixel_color(4, color.rgb(255, 0, 0))
+        print("hot and ")
+    
+        if input.light_level() > 35:
+            light.set_pixel_color(5, color.rgb(255, 255, 0))
+            light.set_pixel_color(6, color.rgb(255, 255, 0))
+            light.set_pixel_color(7, color.rgb(255, 255, 0))
+            light.set_pixel_color(8, color.rgb(255, 255, 0))
+            light.set_pixel_color(9, color.rgb(255, 255, 0))
+            print("sunny")
 
-    elif input.temperature(TemperatureUnit.FAHRENHEIT) < 45 and input.light_level() < 30:
-        light.set_all(color.rgb(0, 0, 255))
-        print("cold and cloudy")
+        else:
+            light.set_pixel_color(5, color.rgb(128, 128, 128))
+            light.set_pixel_color(6, color.rgb(128, 128, 128))
+            light.set_pixel_color(7, color.rgb(128, 128, 128))
+            light.set_pixel_color(8, color.rgb(128, 128, 128))
+            light.set_pixel_color(9, color.rgb(128, 128, 128))
+            print("cloudy")
+
+    elif input.temperature(TemperatureUnit.FAHRENHEIT) < 55:
+        light.set_pixel_color(0, color.rgb(0, 0, 255))
+        light.set_pixel_color(1, color.rgb(0, 0, 255))
+        light.set_pixel_color(2, color.rgb(0, 0, 255))
+        light.set_pixel_color(3, color.rgb(0, 0, 255))
+        light.set_pixel_color(4, color.rgb(0, 0, 255))
+        print("cold and ")
+        
+        if input.light_level() > 35:
+            light.set_pixel_color(5, color.rgb(255, 255, 0))
+            light.set_pixel_color(6, color.rgb(255, 255, 0))
+            light.set_pixel_color(7, color.rgb(255, 255, 0))
+            light.set_pixel_color(8, color.rgb(255, 255, 0))
+            light.set_pixel_color(9, color.rgb(255, 255, 0))
+            print("sunny")
+
+        else:
+            light.set_pixel_color(5, color.rgb(128, 128, 128))
+            light.set_pixel_color(6, color.rgb(128, 128, 128))
+            light.set_pixel_color(7, color.rgb(128, 128, 128))
+            light.set_pixel_color(8, color.rgb(128, 128, 128))
+            light.set_pixel_color(9, color.rgb(128, 128, 128))
+            print("cloudy")
+
 
     else:
-        light.set_all(color.rgb(0, 255, 0))
-        print("nice weather")
+        light.set_pixel_color(0, color.rgb(0, 255, 0))
+        light.set_pixel_color(1, color.rgb(0, 255, 0))
+        light.set_pixel_color(2, color.rgb(0, 255, 0))
+        light.set_pixel_color(3, color.rgb(0, 255, 0))
+        light.set_pixel_color(4, color.rgb(0, 255, 0))
+        print("nice and")
 
-        
+        if input.light_level() > 35:
+            light.set_pixel_color(5, color.rgb(255, 255, 0))
+            light.set_pixel_color(6, color.rgb(255, 255, 0))
+            light.set_pixel_color(7, color.rgb(255, 255, 0))
+            light.set_pixel_color(8, color.rgb(255, 255, 0))
+            light.set_pixel_color(9, color.rgb(255, 255, 0))
+            print("sunny")
+
+        else:
+            light.set_pixel_color(5, color.rgb(128, 128, 128))
+            light.set_pixel_color(6, color.rgb(128, 128, 128))
+            light.set_pixel_color(7, color.rgb(128, 128, 128))
+            light.set_pixel_color(8, color.rgb(128, 128, 128))
+            light.set_pixel_color(9, color.rgb(128, 128, 128))
+            print("cloudy")
+
+
+       
--- main.ts
+++ main.ts
@@ -1,15 +1,75 @@
 while (true) {
     console.log("temperature " + input.temperature(TemperatureUnit.Fahrenheit))
     console.log("light level " + input.lightLevel())
-    if (input.temperature(TemperatureUnit.Fahrenheit) > 80 && input.lightLevel() > 50) {
-        light.setAll(color.rgb(255, 0, 0))
-        console.log("hot and sunny")
-    } else if (input.temperature(TemperatureUnit.Fahrenheit) < 45 && input.lightLevel() < 30) {
-        light.setAll(color.rgb(0, 0, 255))
-        console.log("cold and cloudy")
+    if (input.temperature(TemperatureUnit.Fahrenheit) > 70) {
+        light.setPixelColor(0, color.rgb(255, 0, 0))
+        light.setPixelColor(1, color.rgb(255, 0, 0))
+        light.setPixelColor(2, color.rgb(255, 0, 0))
+        light.setPixelColor(3, color.rgb(255, 0, 0))
+        light.setPixelColor(4, color.rgb(255, 0, 0))
+        console.log("hot and ")
+        if (input.lightLevel() > 35) {
+            light.setPixelColor(5, color.rgb(255, 255, 0))
+            light.setPixelColor(6, color.rgb(255, 255, 0))
+            light.setPixelColor(7, color.rgb(255, 255, 0))
+            light.setPixelColor(8, color.rgb(255, 255, 0))
+            light.setPixelColor(9, color.rgb(255, 255, 0))
+            console.log("sunny")
+        } else {
+            light.setPixelColor(5, color.rgb(128, 128, 128))
+            light.setPixelColor(6, color.rgb(128, 128, 128))
+            light.setPixelColor(7, color.rgb(128, 128, 128))
+            light.setPixelColor(8, color.rgb(128, 128, 128))
+            light.setPixelColor(9, color.rgb(128, 128, 128))
+            console.log("cloudy")
+        }
+        
+    } else if (input.temperature(TemperatureUnit.Fahrenheit) < 55) {
+        light.setPixelColor(0, color.rgb(0, 0, 255))
+        light.setPixelColor(1, color.rgb(0, 0, 255))
+        light.setPixelColor(2, color.rgb(0, 0, 255))
+        light.setPixelColor(3, color.rgb(0, 0, 255))
+        light.setPixelColor(4, color.rgb(0, 0, 255))
+        console.log("cold and ")
+        if (input.lightLevel() > 35) {
+            light.setPixelColor(5, color.rgb(255, 255, 0))
+            light.setPixelColor(6, color.rgb(255, 255, 0))
+            light.setPixelColor(7, color.rgb(255, 255, 0))
+            light.setPixelColor(8, color.rgb(255, 255, 0))
+            light.setPixelColor(9, color.rgb(255, 255, 0))
+            console.log("sunny")
+        } else {
+            light.setPixelColor(5, color.rgb(128, 128, 128))
+            light.setPixelColor(6, color.rgb(128, 128, 128))
+            light.setPixelColor(7, color.rgb(128, 128, 128))
+            light.setPixelColor(8, color.rgb(128, 128, 128))
+            light.setPixelColor(9, color.rgb(128, 128, 128))
+            console.log("cloudy")
+        }
+        
     } else {
-        light.setAll(color.rgb(0, 255, 0))
-        console.log("nice weather")
+        light.setPixelColor(0, color.rgb(0, 255, 0))
+        light.setPixelColor(1, color.rgb(0, 255, 0))
+        light.setPixelColor(2, color.rgb(0, 255, 0))
+        light.setPixelColor(3, color.rgb(0, 255, 0))
+        light.setPixelColor(4, color.rgb(0, 255, 0))
+        console.log("nice and")
+        if (input.lightLevel() > 35) {
+            light.setPixelColor(5, color.rgb(255, 255, 0))
+            light.setPixelColor(6, color.rgb(255, 255, 0))
+            light.setPixelColor(7, color.rgb(255, 255, 0))
+            light.setPixelColor(8, color.rgb(255, 255, 0))
+            light.setPixelColor(9, color.rgb(255, 255, 0))
+            console.log("sunny")
+        } else {
+            light.setPixelColor(5, color.rgb(128, 128, 128))
+            light.setPixelColor(6, color.rgb(128, 128, 128))
+            light.setPixelColor(7, color.rgb(128, 128, 128))
+            light.setPixelColor(8, color.rgb(128, 128, 128))
+            light.setPixelColor(9, color.rgb(128, 128, 128))
+            console.log("cloudy")
+        }
+        
     }
     
 }
